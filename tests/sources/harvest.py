28086226442330d22fcf980909e8e913bc13058f
Byron Ruth <b@devel.io>
2014-12-22 08:40:01 -0500

    Mock Harvest API requests
    Signed-off-by: Byron Ruth <b@devel.io>

--- tests/sources/harvest.py
+++ tests/sources/harvest.py
@@ -1,10 +1,28 @@
+import responses
 from .base import SourceTestCase
 
 
 class TestCase(SourceTestCase):
+    maxDiff = None
     generator = 'harvest'
 
+    @responses.activate
     def test(self):
+        # Mock API root and concepts
+        with open(self.input_path('harvest_api.json')) as f:
+            responses.add(responses.GET,
+                          url='http://harvest.research.chop.edu/demo/api/',
+                          status=200,
+                          body=f.read(),
+                          content_type='application/json')
+
+        with open(self.input_path('harvest_api_concepts.json')) as f:
+            responses.add(responses.GET,
+                          url='http://harvest.research.chop.edu/demo/api/concepts/',
+                          status=200,
+                          body=f.read(),
+                          content_type='application/json')
+
         client = self.module.Client(url='http://harvest.research.chop.edu/demo/api/')
         output = client.generate()
 
